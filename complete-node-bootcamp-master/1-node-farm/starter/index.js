16cb14b9c8a76792c1dff52f55167f7f92ed8577
teng <wwwsousoucom@126.com>
2020-04-16 11:27:15 +0800

    asyn

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -9,8 +9,17 @@ fs.writeFileSync('./txt/output.txt', textOut)
 console.log('File written!')
 
 // Non-blocking asynchronous way
-fs.readFile('./txt/start.txt', (err, data) => {
-    
+fs.readFile('./txt/start.txt', (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2)
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+            console.log(data3)
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('You file has been written Ｏ（∩＿∩）Ｏ！')
+            })
+        })
+    })
 })
 
 console.log('Will read file!')
+
