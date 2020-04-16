4973b869734518eecc8d17079e3fe8d1242fa01b
teng <wwwsousoucom@126.com>
2020-04-16 16:23:36 +0800

    web

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,9 +1,8 @@
 const fs = require('fs')
 const http = require('http')
-
+const url = require('url')
 
 // File
-
 // Blocking synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
 // console.log(textIn)
@@ -26,13 +25,23 @@ const http = require('http')
 // })
 //
 // console.log('Will read file!')
-
 // SERVER
 const server = http.createServer((req, res) => {
-    // console.log(req)
-    res.end('Hello from the server!')
+    const pathName = req.url
+    if (pathName === '/overview') {
+        res.end('This is the OVERVIEW')
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT')
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        })
+        res.end('<h1>Page not found!</h1>')
+    }
 })
 
 server.listen(8000, '127.0.0.1', ()=> {
     console.log('Listening to requests on port 8000')
 })
+
