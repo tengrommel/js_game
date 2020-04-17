a5d49e78e4db2116537d747498373fd2d52d0161
teng <wwwsousoucom@126.com>
2020-04-17 19:39:07 +0800

    slugify

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const http = require('http')
 const url = require('url')
+const slugify = require('slugify')
 const replaceTemplate = require('./modules/replaceTemplate')
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
@@ -8,6 +9,9 @@ const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'u
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
+
+const slugs = dataObj.map(el => slugify(el.productName, {lower: true}))
+console.log(slugs)
 
 const server = http.createServer((req, res) => {
     const {query, pathname} = url.parse(req.url, true)
