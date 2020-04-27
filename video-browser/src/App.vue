3c5f45b45ae0d2997c6b247dff21042031d7af42
teng <wwwsousoucom@126.com>
2020-04-27 18:12:35 +0800

    axios

--- video-browser/src/App.vue
+++ video-browser/src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="W to Your Vue.js App"/>
-  </div>
+    <div>
+        <SearchBar @termChange="onTermChange"></SearchBar>
+        <VideoList></VideoList>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import SearchBar from "./components/SearchBar";
+    import axios from 'axios';
+    import VideoList from "./components/VideoList";
+    const API_KEY = 'AIzaSyCrFzAjyhHGpCWbPZU8HAm-BDHzDF4BP7s';
+    export default {
+        name: 'App',
+        components: {
+            VideoList,
+            SearchBar
+        },
+        methods: {
+            onTermChange(searchTerm) {
+                axios.get('https://www.googleapis.com/youtube/v3/search', {
+                    params: {
+                        key: API_KEY,
+                        type: 'video',
+                        part: 'snippet',
+                        q: searchTerm
+                    }
+                }).then(response => console.log(response))
+            }
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
