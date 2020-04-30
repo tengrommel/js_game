6e2d8f91003653e56179fbd026eb01d320f72928
teng <wwwsousoucom@126.com>
2020-04-30 20:40:18 +0800

    wasm ok

--- 3d-demo/index.js
+++ 3d-demo/index.js
@@ -1,4 +1,40 @@
-const rust = import('./pkg/rust_3d_demo');
+const rust = import("./pkg/rust_3d_demo");
+const canvas = document.getElementById("rustCanvas");
+const gl = canvas.getContext("webgl", { antialias: true });
 
-rust.then(m => m.say_hello_from_rust())
-    .catch(console.error);
+rust.then((m) => {
+  if (!gl) {
+    alert("Failed to initialize WebGL");
+    return;
+  }
+
+  gl.enable(gl.BLEND);
+  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_CONSTANT_ALPHA);
+
+  const FPS_THROTTLE = 1000.0/30.0; // milliseconds / frames
+  const dougsClient = new m.DougsClient();
+  const initialTime = Date.now();
+  let lastDrawTime = -1;
+
+  function render() {
+    window.requestAnimationFrame(render);
+    const currTime = Date.now();
+    if (currTime >= lastDrawTime + FPS_THROTTLE) {
+      lastDrawTime = currTime;
+      if (window.innerHeight !== canvas.height || window.innerWidth !== canvas.width) {
+        canvas.height = window.innerHeight;
+        canvas.clientHeight = window.innerHeight;
+        canvas.style.height = window.innerHeight;
+
+        canvas.width = window.innerWidth;
+        canvas.clientWidth = window.innerWidth;
+        canvas.style.width = window.innerWidth;
+        gl.viewport(0, 0, window.innerWidth, window.innerHeight);
+      }
+      let elapsedTime = currTime - initialTime;
+      dougsClient.update(elapsedTime, window.innerHeight, window.innerWidth);
+      dougsClient.render();
+    }
+  }
+  render();
+});
--- 3d-demo/src/lib.rs
+++ 3d-demo/src/lib.rs
@@ -11,3 +11,26 @@ extern "C" {
 pub fn say_hello_from_rust() {
     log("Howdy!... from Rust");
 }
+
+#[wasm_bindgen]
+pub struct DougsClient {
+
+}
+
+#[wasm_bindgen]
+impl DougsClient {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        log("New was hit");
+        Self {
+
+        }
+    }
+    pub fn update(&mut self, _time: f32, _height: f32, _width: f32) -> Result<(), JsValue> {
+        Ok(())
+    }
+
+    pub fn render(&self) {
+        log("render was hit");
+    }
+}
