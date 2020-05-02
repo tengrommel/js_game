42a4b7e5d8af300f16e987bd53075eec09b3175b
teng <wwwsousoucom@126.com>
2020-05-02 15:02:19 +0800

    index

---
+++ bible/index.js
@@ -0,0 +1,64 @@
+/**
+ * EXAMPLE 1 - Variable declaration
+ */
+// var a;
+
+// console.log(a);
+// // console.log(b);
+// // Uncaught ReferenceError: b is not defined
+// // at index.js:7
+
+// let c;
+
+// console.log(c);
+// // const d;Uncaught SyntaxError: Missing initializer in const declaration
+
+// // declaration
+// let myNumber = 10;
+// console.log(myNumber);
+
+// // assignment
+// let myString;
+// console.log(myString);
+
+// // re-assignment
+// myString = "Hello from string";
+// console.log(myString);
+
+// // 
+// myString = "New value for myString";
+// console.log(myString);
+
+/**
+ * EXAMPLE 3 - "var"
+ */
+
+// declaration
+// var myNumber = 10;
+// console.log(myNumber);
+
+// // assignment
+// var myString;
+// console.log(myString);
+
+// // re-assignment
+// myString = "Hello from string";
+// console.log(myString);
+
+// myString = "New value for myString";
+// console.log(myString);
+
+// /**
+//  * EXAMPLE 4 - "const"
+//  */
+// const myBoolean = true;
+// console.log(myBoolean);
+
+// // const myBoolean = false; Uncaught SyntaxError: Identifier 'myBoolean' has already been declared
+
+/**
+ * CHALLENGE 1
+ * Declare variable "myObject" and assign value {}.
+ * *Print this variable to the console
+ * NOTE: variable "myObject" will NOT reassigned in the future
+ */
